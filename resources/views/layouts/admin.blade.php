03e52a471c14cbbf86ecbea7c544f6ffd358b8c2
alexis2608 <colores.2808.8000@gmail.com>
2025-05-27 13:43:15 -0600

    Logo y favicon

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
     <title>{{ config('app.name', 'Admin') }}</title>
 
     <!-- Fonts -->
